8a64fdd8111975fcfcddde51c9e418fc1899efc4
romrap <raphael24@gmx.ch>
2024-04-15 16:30:59 +0200

    add headerfile

---
+++ main.h
@@ -0,0 +1,47 @@
+#pragma once 
+
+#include <cstdio>
+#include <mbed.h>
+#include <ctime>
+
+#include "pm2_drivers/DebounceIn.h"
+
+
+
+# define M_PI 3.14159265358979323846 // number pi, an example in case you need it
+
+// ---- Vehicle Variables ----
+//# define WHEEL_DIAMETER 30.0 
+# define GEAR_DIAMETER 20.3
+
+
+bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and decides whether to execute the main task or not
+bool do_reset_all_once = false;    // this variable is used to reset certain variables and objects and shows how you can run a code segment only once
+bool start_pressed = false;
+
+// objects for user button (blue button) handling on nucleo board
+//DebounceIn user_button(PC_13);  // create InterruptIn interface object to evaluate user button falling and rising edge (no blocking code in ISR)
+DebounceIn user_button(PB_12);  // create InterruptIn interface object to evaluate user button falling and rising edge (no blocking code in ISR)
+
+
+
+// define functions
+
+bool read_cap_coulor(void);
+bool drive_belt(void);
+bool read_liquid_level(void);
+int drive_belt(int velocity, int cylecounter);
+void show_LED(int error_code);
+
+
+
+
+
+
+
+
+
+
+
+
+
